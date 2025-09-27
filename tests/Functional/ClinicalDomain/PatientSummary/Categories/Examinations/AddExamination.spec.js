0f513b971e398bc14377268dc62de98f9404cacb
manoj.vyavahare <manoj.vyavahare@riomed.com>
2025-09-27 22:33:32 +0530

    remove click home icon

--- tests/Functional/ClinicalDomain/PatientSummary/Categories/Examinations/AddExamination.spec.js
+++ tests/Functional/ClinicalDomain/PatientSummary/Categories/Examinations/AddExamination.spec.js
@@ -104,7 +104,7 @@ test.describe("Examination Category", () => {
 
 
       logger.info("Clicked on Login button successfully");
-      await homepage.clickOnHomeDashboardIcon()
+      
       await homepage.clickOnPatientIcon();
       logger.info("Clicked on Patient Icon successfully");
       await patientsearch.clickOnSearchButton();
